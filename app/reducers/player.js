c6c770337adf00e08fc0ea9575259669b2f1e12a
carl.hand <carl.a.hand92@gmail.com>
2017-04-18 12:19:50 +0100

    created switch function for reducer with case for adding a player

--- app/reducers/player.js
+++ app/reducers/player.js
@@ -1,3 +1,30 @@
 /**
  * Created by carl.hand on 18/04/2017.
  */
+import * as PlayerActionTypes from '../actiontypes/player';
+
+const initialState = [
+    {
+        name: "Carl",
+        score: 0,
+    },
+    {
+        name: "Lee",
+        score: 0,
+    },
+];
+
+export default function Player(state=initialState, action) {
+    switch(action.type) {
+        case PlayerActionTypes.ADD:
+            return [
+                ...state,
+                {
+                    name: action.name,
+                    score: 0
+                }
+            ];
+        default:
+            return state;
+    }
+}
